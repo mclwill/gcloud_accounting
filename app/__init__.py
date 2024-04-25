7c2e14789439066763b832c6d84d3d7d243ab9e3
mclwill <gary@mclarenwilliams.com.au>
2024-04-26 08:47:24 +1000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,4 @@ from FlaskApp.app import views
 with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
     print('__init__.py')
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
-with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
-    print('run.py in __init__.py')
