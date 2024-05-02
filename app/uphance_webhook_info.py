6bfacd5cec0e59cac3351d3400168c33825678d0
mclwill <gary@mclarenwilliams.com.au>
2024-05-02 16:49:51 +1000

    fix country codes .csv

--- app/uphance_webhook_info.py
+++ app/uphance_webhook_info.py
@@ -47,7 +47,7 @@ aemery_dbx_app_secret = 'in08qrhgq0qit5n'
 
 #dbx_folder = "/A.Emery/Wholesale/APIs (Anna's Dad)/Cross Docks Info/FTP_production_files/sent/"
 
-cc_codes_pd = pd.read_csv('CountryCodes.csv',index_col='Country')
+cc_codes_pd = pd.read_csv('FlaskApp/app/CountryCodes.csv',index_col='Country')
 
 
 '''def send_email(message_subject,message_text,receiver_email_address):
