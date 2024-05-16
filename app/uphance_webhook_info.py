d50d0c56b8906ddcd5b761d3bbe2976ba3b44c81
mclwill <gary@mclarenwilliams.com.au>
2024-05-16 13:19:01 +1000

    correct username in FTP login credentials

--- app/uphance_webhook_info.py
+++ app/uphance_webhook_info.py
@@ -87,7 +87,7 @@ cc_codes_pd = pd.read_csv('/var/www/FlaskApp/FlaskApp/app/CountryCodes.csv',inde
 def transfer_FTP(customer,file_name,file_data):
     cross_docks_info = common.get_CD_FTP_credentials(customer)
     try: 
-        with ftputil.FTPHost("ftp.crossdocks.com.au", cross_docks_info[username], common.cross_docks_info[password]) as ftp_host:
+        with ftputil.FTPHost("ftp.crossdocks.com.au", cross_docks_info['username'], common.cross_docks_info['password']) as ftp_host:
 
             ftp_host.chdir('in/pending')
             with ftp_host.open(file_name, "w", encoding="utf8") as fobj:
