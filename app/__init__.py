f26fa068a92a43aee05985e976c6fe16ffa1bd45
mclwill <gary@mclarenwilliams.com.au>
2024-08-18 14:59:02 +1000

    new dashboard.py module

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,15 @@ import dash_html_components as html
 import sys
 
 app = Flask(__name__)
+dash_app = dash.Dash(server=app,routes_pathname_prefix="/dashboard/")
 
 from FlaskApp.app import views
+from FlaskApp.app import dashboard
 
 with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
     print('__init__.py')
 
-dash_app = dash.Dash(server=app,routes_pathname_prefix="/dashboard/")
 
-dash_app.layout = html.Div("This is the Dash app")
 
 if __name__ == "__main__":
     dash_app.run_server(debug=True)
---
+++ app/dashboard.py
@@ -0,0 +1,3 @@
+from FlaskApp.app import dash_app
+
+dash_app.layout = html.Div("This is the Dash app")
