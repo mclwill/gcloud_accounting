201e633ba848bf4fe74507fb5cf4c772728bb9fa
mclwill <gary@mclarenwilliams.com.au>
2024-08-18 14:01:32 +1000

    change name to data_store

---
+++ app/data_store.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import FlaskApp.app.common as common
+from datetime import datetime
+'''
+module for collecting daily information on stock levels, sales, returns and new stock orders
+Columns
+
+Date,p_id,p_identifier,p_url,p_name,season_id,sku_id,sku_number,color,size,ean,in_stock,available_to_sell,available_to_sell_from_stock
+'''
+
+import cross_docks_polling as cd_polling
+
+utc_zone = tz.tzutc()
+to_zone = tz.gettz('Australia/Melbourne')
+
+stock_file_path = "/Wholesale/APIs (Anna's Dad)/Uphance/DataStore/data_stock.csv"
+orders_file_path = "/Wholesale/APIs (Anna's Dad)/Uphance/DataStore/data_orders.csv"
+
+def get_uphance_stock_levels(customer):
+	global stock_file_path
+	byte_stream = common.read_dropbox_bytestream(customer,stock_file_path)
+	if byte_stream:
+		df = pd.read_csv(byte_stream,sep='|',index_col=False)
+	else:
+		df = pd.DataFrame() #start with empty dataframe
+	
+	url_product = 'https://api.uphance.com/products'
+	page = 1 #get ready for Uphance pagination
+	while page :
+	    common.logger.debug('Request product dump from Uphance - Page : ' + str(page))
+	    response = common.uphance_api_call(customer,'get',url=url_product+'/?page='+str(page))
+	    common.logger.debug('Uphance Product API Call Status Code: ' + str(response[0]))
+	    if response[0]:
+	    	common.logger.warning('Uphance Error on Product API call for :' + customer)
+	    	break
+	    else:
+		    data = response[1]
+		    for p in data['products']:
+		        row_dict = {}
+		        row_dict['Date'] = [datetime.now().replace(tzinfo=utc_zone).astimezone(to_zone).replace(tzinfo=None)]
+		        row_dict['p_id'] = [p['id']]
+		        row_dict['p_identifier'] = [p['product_identifier']]
+		        row_dict['p_name'] = [p['name']]
+		        row_dict['url'] = [p['image_url']]
+		        row_dict['season_id'] = [','.join(str(x) for x in p['season_id'])]
+		        row_dict['category'] = [p['category']]
+		        row_dict['sub_category'] = [p['sub_category']]
+		        for v in p['variations'] :
+		            row_dict['color'] = [v['color']]
+		            for pr in v['prices']:
+		                row_dict['price_' + pr['name'] + '_currency' ] = [pr['currency']]
+		                row_dict['price_' + pr['name'] + '_wsp' ] = [pr['wsp_money']]
+		                row_dict['price_' + pr['name'] + '_mrsp' ] = [pr['msrp_money']]
+		            for sku in v['skus']:    
+		                row_dict['sku_id'] = [sku['id']]
+		                row_dict['size'] = [sku['size']]
+		                row_dict['sku_number'] = [sku['sku_number']]
+		                row_dict['ean'] = [sku['ean']]
+		                row_dict['in_stock'] = [sku['in_stock']]
+		                row_dict['available_to_sell'] = [sku['available_to_sell']]
+		                row_dict['available_to_sell_from_stock'] = [sku['available_to_sell_from_stock']]
+		        df = pd.concat([df,pd.DataFrame.from_dict(row_dict)])
+	    page = data['meta']['next_page']
+	csv_file_data = df.to_csv(sep='|',index=False)
+	common.store_dropbox_unicode(customer,csv_file_data,stock_file_path)
+	common.logger.info('Uphance stock DataStore updated for ' + customer + '\nFile Path: ' + stock_file_path)
+
+
+def or_file_sent(customer,file_data):
+	global orders_file_path
+
+	byte_stream = common.read_dropbox_bytestream(customer,orders_file_path)
+	if byte_stream:
+		df = pd.read_csv(byte_stream,sep='|',index_col=False)
+	else:
+		df = pd.DataFrame() #start with empty dataframe
+
+	data_lines = file_data.split('\n')
+	channel = cd_polling.get_CD_parameter(data_lines,'OR1',12)
+	order_id = cd_polling.get_CD_parameter(data_lines,'OR1',3)
+	eans = cd_polling.get_CD_parameter(data_lines,'OR2',4)
+	qty_ordered = cd_polling.get_CD_parameter(data_lines,'OR2',7)
+	
+	for i in len(eans):
+		row_dict = {}
+		row_dict['date_ordered'] = [datetime.now().replace(tzinfo=utc_zone).astimezone(to_zone).replace(tzinfo=None)]
+		row_dict['order_id'] = [order_id]
+		row_dict['channel'] = [channel]
+		row_dict['ean'] = [eans[i]]
+		row_dict['qty_ordered'] = [qty_ordered[i]]
+
+	df = pd.concat([df,pd.DataFrame.from_dict(row_dict)])
+	csv_file_data = df.to_csv(sep='|',index=False)
+	common.store_dropbox_unicode(customer,csv_file_data,orders_file_path)
+	common.logger.info('Uphance orders DataStore updated for ' + customer + '\nFile Path: ' + orders_file_path)
+
+def pc_file_received(customer,data_lines):
+	global orders_file_path
+
+	byte_stream = common.read_dropbox_bytestream(customer,orders_file_path)
+	if byte_stream:
+		df = pd.read_csv(byte_stream,sep='|',index_col=False)
+	else:
+		df = pd.DataFrame() #start with empty dataframe
+
+	data_lines = file_data.split('\n')
+	order_id = cd_polling.get_CD_parameter(data_lines,'OS1',2)
+	eans = cd_polling.get_CD_parameter(data_lines,'OS2',2)
+	qty_shipped = cd_polling.get_CD_parameter(data_lines,'OR2',4)
+	qty_variance =cd_polling.get_CD_parameter(data_lines,'OR2',5)
+	
+	for i in len(eans):
+		row_dict = {}
+		row_dict['order_id'] = [order_id]
+		row_dict['date_shipped'] = [datetime.now().replace(tzinfo=utc_zone).astimezone(to_zone).replace(tzinfo=None)]
+		row_dict['ean'] = [eans[i]]
+		row_dict['qty_ordered'] = [qty_ordered[i]]
+
+	df = pd.concat([df,pd.DataFrame.from_dict(row_dict)])
+	csv_file_data = df.to_csv(sep='|',index=False)
+	common.store_dropbox_unicode(customer,csv_file_data,orders_file_path)
+	common.logger.info('Uphance orders DataStore updated for ' + customer + '\nFile Path: ' + orders_file_path)
