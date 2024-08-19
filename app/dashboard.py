539bbe6d40033c937a456d399ee5780bbfddf822
mclwill <gary@mclarenwilliams.com.au>
2024-08-19 18:03:34 +1000

    add error handling to dashboard

--- app/dashboard.py
+++ app/dashboard.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output
 from dash_table import DataTable
 from dash.exceptions import PreventUpdate
 import plotly.express as px
+import traceback
 
 import FlaskApp.app.common as common
 
@@ -32,56 +33,79 @@ for view_func in app.view_functions:
         app.view_functions[view_func] = login_required(app.view_functions[view_func])
 
 def serve_layout():
-    #collect data in serve_layout so that latest is retrieved from data_store
-    global available_columns,available_products,available_colors,available_sizes
-    global product_option_list,color_option_list,size_option_list
+    try:
+        #collect data in serve_layout so that latest is retrieved from data_store
+        global available_columns,available_products,available_colors,available_sizes
+        global product_option_list,color_option_list,size_option_list
 
-    byte_stream = common.read_dropbox_bytestream(customer,stock_file_path)
-    if byte_stream:
-        df = pd.read_csv(byte_stream,sep='|',index_col=False)
-    else:
-        df = pd.DataFrame() #start with empty dataframe
+        byte_stream = common.read_dropbox_bytestream(customer,stock_file_path)
+        if byte_stream:
+            df = pd.read_csv(byte_stream,sep='|',index_col=False)
+        else:
+            df = pd.DataFrame() #start with empty dataframe
 
-    df['url_markdown'] = df['url'].map(lambda a : "[![Image Not Available](" + str(a) + ")](https://aemery.com)")
-
-    available_columns = df[['url_markdown','date','season','p_name','color','size','sku_id','in_stock','available_to_sell','available_to_sell_from_stock']]
-    col_title_mapping = {'url_markdown':'Image','date':'Date','season':'Season(s)','p_name':'Product','color':'Colour','size':'Size','sku_id':'SKU','in_stock':'In Stock','available_to_sell':'Available To Sell','available_to_sell_from_stock':'Available To Sell From Stock'}
-    available_columns = available_columns[available_columns['date'] == available_columns['date'].max()]
-    available_products = df['p_name'].unique()
-    available_colors = df['color'].unique()
-    available_sizes = df['size'].unique()
-
-    product_option_list = sorted(available_columns['p_name'].unique().tolist())
-    color_option_list = sorted(available_columns['color'].unique().tolist())
-    size_option_list = sorted(available_columns['size'].unique().tolist())
-
-    return html.Div([
-        dbc.Row([
-            dbc.Col(
-                dbc.Card([
-                    dbc.CardBody([
-                        html.H1("Dashboard"),
-                        html.P('''
-                             This is a dashboard for A.Emery
-                             '''),
-                    ]),   
-                ],className="border-0 bg-transparent"),
-                width={"size":4}
-            ),
-            dbc.Col(
-                dbc.Button("Logout",href='/logout',color='light',size='lg',external_link=True,),
-                width={"size":1,'offset':12}
+        if df.empty()
+            return html.Div(
+                html.P('No Data to Display - need to check Data Store')
             )
-        ],justify='evenly'),
-        dbc.Row([
-            dbc.Col(
+
+        df['url_markdown'] = df['url'].map(lambda a : "[![Image Not Available](" + str(a) + ")](https://aemery.com)")
+
+        available_columns = df[['url_markdown','date','season','p_name','color','size','sku_id','in_stock','available_to_sell','available_to_sell_from_stock']]
+        col_title_mapping = {'url_markdown':'Image','date':'Date','season':'Season(s)','p_name':'Product','color':'Colour','size':'Size','sku_id':'SKU','in_stock':'In Stock','available_to_sell':'Available To Sell','available_to_sell_from_stock':'Available To Sell From Stock'}
+        available_columns = available_columns[available_columns['date'] == available_columns['date'].max()]
+        available_products = df['p_name'].unique()
+        available_colors = df['color'].unique()
+        available_sizes = df['size'].unique()
+
+        product_option_list = sorted(available_columns['p_name'].unique().tolist())
+        color_option_list = sorted(available_columns['color'].unique().tolist())
+        size_option_list = sorted(available_columns['size'].unique().tolist())
+
+        return html.Div([
+            dbc.Row([
+                dbc.Col(
                     dbc.Card([
                         dbc.CardBody([
-                            html.P("Product"),
+                            html.H1("Dashboard"),
+                            html.P('''
+                                 This is a dashboard for A.Emery
+                                 '''),
+                        ]),   
+                    ],className="border-0 bg-transparent"),
+                    width={"size":4}
+                ),
+                dbc.Col(
+                    dbc.Button("Logout",href='/logout',color='light',size='lg',external_link=True,),
+                    width={"size":1,'offset':12}
+                )
+            ],justify='evenly'),
+            dbc.Row([
+                dbc.Col(
+                        dbc.Card([
+                            dbc.CardBody([
+                                html.P("Product"),
+                                html.Div([
+                                    dcc.Dropdown(
+                                        id='product_option',
+                                        options=product_option_list,
+                                        value=[],
+                                        placeholder = 'All',
+                                        multi = True,
+                                        clearable = True
+                                    ),
+                                ]),
+                            ]),
+                        ],className="border-0 bg-transparent"),
+                ),
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.P("Colour"),
                             html.Div([
                                 dcc.Dropdown(
-                                    id='product_option',
-                                    options=product_option_list,
+                                    id='color_option',
+                                    options=color_option_list,
                                     value=[],
                                     placeholder = 'All',
                                     multi = True,
@@ -90,61 +114,50 @@ def serve_layout():
                             ]),
                         ]),
                     ],className="border-0 bg-transparent"),
-            ),
-            dbc.Col(
-                dbc.Card([
-                    dbc.CardBody([
-                        html.P("Colour"),
-                        html.Div([
-                            dcc.Dropdown(
-                                id='color_option',
-                                options=color_option_list,
-                                value=[],
-                                placeholder = 'All',
-                                multi = True,
-                                clearable = True
-                            ),
+                ),
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.P("Size"),
+                            html.Div([
+                                dcc.Dropdown(
+                                    id='size_option',
+                                    options=size_option_list,
+                                    value=[],
+                                    placeholder = 'All',
+                                    multi = True,
+                                    clearable = True
+                                ),
+                            ]),
                         ]),
-                    ]),
-                ],className="border-0 bg-transparent"),
-            ),
-            dbc.Col(
-                dbc.Card([
-                    dbc.CardBody([
-                        html.P("Size"),
-                        html.Div([
-                            dcc.Dropdown(
-                                id='size_option',
-                                options=size_option_list,
-                                value=[],
-                                placeholder = 'All',
-                                multi = True,
-                                clearable = True
-                            ),
-                        ]),
-                    ]),
-                ],className="border-0 bg-transparent"),
-            ),
-        ]),
-        dbc.Row([
-            dbc.Card([
-                dbc.CardBody([
-                    dash_table.DataTable(
-                        id='data_table',
-                        columns=[{"name": col_title_mapping[i], "id": i, 'presentation':'markdown'} if ('markdown' in i) else {"name": col_title_mapping[i], "id": i} for i in available_columns.columns],
-                        data=available_columns.to_dict("records"),
-                        style_cell_conditional = [
-                            {
-                                'if':{'column_id':i},
-                                'textAlign':'center'
-                            } for i in ['url_markdown']
-                        ],
-                        css=[dict(selector= "p", rule= "margin: 0; text-align: center")]
-                    )
-                ]),
+                    ],className="border-0 bg-transparent"),
+                ),
             ]),
+            dbc.Row([
+                dbc.Card([
+                    dbc.CardBody([
+                        dash_table.DataTable(
+                            id='data_table',
+                            columns=[{"name": col_title_mapping[i], "id": i, 'presentation':'markdown'} if ('markdown' in i) else {"name": col_title_mapping[i], "id": i} for i in available_columns.columns],
+                            data=available_columns.to_dict("records"),
+                            style_cell_conditional = [
+                                {
+                                    'if':{'column_id':i},
+                                    'textAlign':'center'
+                                } for i in ['url_markdown']
+                            ],
+                            css=[dict(selector= "p", rule= "margin: 0; text-align: center")]
+                        )
+                    ]),
+                ]),
+            ])
         ])
-    ])  
+    except Exception as e:
+        tb = traceback.format_exc()
+        common.logger.warning('Error Process Dashboard Layout' + '/nException Info: ' + str(ex) + '/nTraceback Info: ' + str(tb))
+        return html.Div(
+                html.P('Error processing layout')
+        ) 
 
 @dash_app.callback(
     Output('color_option', 'options'),
