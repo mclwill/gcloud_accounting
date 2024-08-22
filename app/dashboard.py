10fcf158738757c4778bf1869398d9d985c8bd95
mclwill <gary@mclarenwilliams.com.au>
2024-08-22 21:44:09 +1000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -44,7 +44,7 @@ def last_day_of_month(any_day):
     # The day 28 exists in every month. 4 days later, it's always next month
     next_month = any_day.replace(day=28) + timedelta(days=4)
     # subtracting the number of the current day brings us back one month
-    return next_month - datetime.timedelta(days=next_month.day)
+    return next_month - timedelta(days=next_month.day)
 
 def get_start_of_previous_week(date_value):
     weekday = date_value.weekday()
