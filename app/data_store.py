7f71d0159d6ec6e4e18eb382cb56c5a540da71cc
mclwill <gary@mclarenwilliams.com.au>
2024-08-22 10:16:01 +1000

    d

--- app/data_store.py
+++ app/data_store.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, date, time, timedelta
 from dateutil import tz
 import traceback
 
@@ -15,6 +15,12 @@ Date,p_id,p_identifier,p_url,p_name,season_id,sku_id,sku_number,color,size,ean,i
 
 utc_zone = tz.tzutc()
 to_zone = tz.gettz('Australia/Melbourne')
+
+def in_between(now, start, end):
+    if start <= end:
+        return start <= now < end
+    else: # over midnight e.g., 23:30-04:15
+        return start <= now or now < end
 
 def dict_append(d,k,v): #used to build row_dict array in format to transfer to pandas dataframe
     if k in d:
@@ -43,71 +49,75 @@ def get_data_store_info(customer):
         po_file_path = os.path.join(data_store_folder,'data_po.csv')
         orders_retrieve_path = common.access_secret_version('customer_parameters',customer,'dbx_folder')
         
-        #get season data from uphance
-        url_seasons = 'https://api.uphance.com/seasons'
-        response = common.uphance_api_call(customer,'get',url=url_seasons)
-        
-        if response[0]:
-            common.logger.warning('Uphance Error on Season API call for :' + customer)
-            season_df = None
-        else:
-            data = response[1]
-            season_df = pd.DataFrame.from_dict(data['seasons'])
-            season_df['id'] = season_df['id'].astype(str)
-            season_df.set_index('id',inplace=True)
-
-        #get stock level info from Uphance
-
-        byte_stream = common.read_dropbox_bytestream(customer,stock_file_path)
-        if byte_stream:
-            df = pd.read_csv(byte_stream,sep='|',index_col=False,dtype={'in_stock':'Int64','available_to_sell':'Int64','available_to_sell_from_stock':'Int64'})
-        else:
-            df = pd.DataFrame() #start with empty dataframe
         aest_now = datetime.now().replace(tzinfo=utc_zone).astimezone(to_zone).replace(tzinfo=None)
-        url_product = 'https://api.uphance.com/products'
-        page = 1 #get ready for Uphance pagination
-        while page :
-            common.logger.debug('Request product dump from Uphance - Page : ' + str(page))
-            response = common.uphance_api_call(customer,'get',url=url_product+'/?page='+str(page))
-            common.logger.debug('Uphance Product API Call Status Code: ' + str(response[0]))
+        
+        if in_between(aest_now.time(),time(23),time(6)) : #only do update between these times which is likely cronjob triggered rather than manual testing
+
+            #get season data from uphance
+            url_seasons = 'https://api.uphance.com/seasons'
+            response = common.uphance_api_call(customer,'get',url=url_seasons)
+            
             if response[0]:
-                common.logger.warning('Uphance Error on Product API call for :' + customer)
-                break
+                common.logger.warning('Uphance Error on Season API call for :' + customer)
+                season_df = None
             else:
                 data = response[1]
-                row_dict = {}
-                for p in data['products']:
-                    for v in p['variations'] :
-                        for sku in v['skus']:  
-                            if sku['ean']:
-                                row_dict['date'] = dict_append(row_dict,'date',aest_now)
-                                row_dict['p_id'] = dict_append(row_dict,'p_id',p['id'])
-                                row_dict['p_identifier'] = dict_append(row_dict,'p_identifier',p['product_identifier'])
-                                row_dict['p_name'] = dict_append(row_dict,'p_name',p['name'])
-                                row_dict['url'] = dict_append(row_dict,'url',p['image_url'])
-                                row_dict['season_id'] = dict_append(row_dict,'season_id',','.join(str(x) for x in p['season_id']))
-                                row_dict['category'] = dict_append(row_dict,'category',p['category'])
-                                row_dict['sub_category'] = dict_append(row_dict,'sub_category',p['sub_category'])
-                                row_dict['color'] = dict_append(row_dict,'color',v['color'])
-                                row_dict['sku_id'] = dict_append(row_dict,'sku_id',sku['id'])
-                                row_dict['ean'] = dict_append(row_dict,'ean',sku['ean'])
-                                row_dict['size'] = dict_append(row_dict,'size',sku['size'])
-                                row_dict['sku_number'] = dict_append(row_dict,'sku_number',sku['sku_number'])
-                                row_dict['in_stock'] = dict_append(row_dict,'in_stock',sku['in_stock'])
-                                row_dict['available_to_sell'] = dict_append(row_dict,'available_to_sell',sku['available_to_sell'])
-                                row_dict['available_to_sell_from_stock'] = dict_append(row_dict,'available_to_sell_from_stock',sku['available_to_sell_from_stock'])
-                                for pr in v['prices']:
-                                    row_dict['price_' + pr['name'] + '_currency' ] = dict_append(row_dict,'price_' + pr['name'] + '_currency',pr['currency'])
-                                    row_dict['price_' + pr['name'] + '_wsp' ] = dict_append(row_dict,'price_' + pr['name'] + '_wsp',pr['wsp_money'])
-                                    row_dict['price_' + pr['name'] + '_mrsp' ] = dict_append(row_dict,'price_' + pr['name'] + '_mrsp' ,pr['msrp_money'])
-            
-                df = pd.concat([df,pd.DataFrame.from_dict(row_dict)])
-                page = data['meta']['next_page']
-        if season_df is not None:
-            df['season'] = df.apply(lambda row: decode_season_id(row['season_id']),axis=1)
-        csv_file_data = df.to_csv(sep='|',index=False)
-        common.store_dropbox_unicode(customer,csv_file_data,stock_file_path)
-        common.logger.info('Uphance stock DataStore updated for ' + customer + '\nFile Path: ' + stock_file_path)
+                season_df = pd.DataFrame.from_dict(data['seasons'])
+                season_df['id'] = season_df['id'].astype(str)
+                season_df.set_index('id',inplace=True)
+
+            #get stock level info from Uphance
+
+            byte_stream = common.read_dropbox_bytestream(customer,stock_file_path)
+            if byte_stream:
+                df = pd.read_csv(byte_stream,sep='|',index_col=False,dtype={'in_stock':'Int64','available_to_sell':'Int64','available_to_sell_from_stock':'Int64'})
+            else:
+                df = pd.DataFrame() #start with empty dataframe
+
+            url_product = 'https://api.uphance.com/products'
+            page = 1 #get ready for Uphance pagination
+            while page :
+                common.logger.debug('Request product dump from Uphance - Page : ' + str(page))
+                response = common.uphance_api_call(customer,'get',url=url_product+'/?page='+str(page))
+                common.logger.debug('Uphance Product API Call Status Code: ' + str(response[0]))
+                if response[0]:
+                    common.logger.warning('Uphance Error on Product API call for :' + customer)
+                    break
+                else:
+                    data = response[1]
+                    row_dict = {}
+                    for p in data['products']:
+                        for v in p['variations'] :
+                            for sku in v['skus']:  
+                                if sku['ean']:
+                                    row_dict['date'] = dict_append(row_dict,'date',aest_now)
+                                    row_dict['p_id'] = dict_append(row_dict,'p_id',p['id'])
+                                    row_dict['p_identifier'] = dict_append(row_dict,'p_identifier',p['product_identifier'])
+                                    row_dict['p_name'] = dict_append(row_dict,'p_name',p['name'])
+                                    row_dict['url'] = dict_append(row_dict,'url',p['image_url'])
+                                    row_dict['season_id'] = dict_append(row_dict,'season_id',','.join(str(x) for x in p['season_id']))
+                                    row_dict['category'] = dict_append(row_dict,'category',p['category'])
+                                    row_dict['sub_category'] = dict_append(row_dict,'sub_category',p['sub_category'])
+                                    row_dict['color'] = dict_append(row_dict,'color',v['color'])
+                                    row_dict['sku_id'] = dict_append(row_dict,'sku_id',sku['id'])
+                                    row_dict['ean'] = dict_append(row_dict,'ean',sku['ean'])
+                                    row_dict['size'] = dict_append(row_dict,'size',sku['size'])
+                                    row_dict['sku_number'] = dict_append(row_dict,'sku_number',sku['sku_number'])
+                                    row_dict['in_stock'] = dict_append(row_dict,'in_stock',sku['in_stock'])
+                                    row_dict['available_to_sell'] = dict_append(row_dict,'available_to_sell',sku['available_to_sell'])
+                                    row_dict['available_to_sell_from_stock'] = dict_append(row_dict,'available_to_sell_from_stock',sku['available_to_sell_from_stock'])
+                                    for pr in v['prices']:
+                                        row_dict['price_' + pr['name'] + '_currency' ] = dict_append(row_dict,'price_' + pr['name'] + '_currency',pr['currency'])
+                                        row_dict['price_' + pr['name'] + '_wsp' ] = dict_append(row_dict,'price_' + pr['name'] + '_wsp',pr['wsp_money'])
+                                        row_dict['price_' + pr['name'] + '_mrsp' ] = dict_append(row_dict,'price_' + pr['name'] + '_mrsp' ,pr['msrp_money'])
+                
+                    df = pd.concat([df,pd.DataFrame.from_dict(row_dict)])
+                    page = data['meta']['next_page']
+            if season_df is not None:
+                df['season'] = df.apply(lambda row: decode_season_id(row['season_id']),axis=1)
+            csv_file_data = df.to_csv(sep='|',index=False)
+            common.store_dropbox_unicode(customer,csv_file_data,stock_file_path)
+            common.logger.info('Uphance stock DataStore updated for ' + customer + '\nFile Path: ' + stock_file_path)
 
         #get order info from locally stored files
 
@@ -126,8 +136,9 @@ def get_data_store_info(customer):
         queuedFiles = common.get_dropbox_file_info(customer,os.path.join(orders_retrieve_path,'sent'),from_date=datetime.now()-timedelta(days=10)) #use utc time as that is how dropbox stores file dates
         queuedFiles = queuedFiles + common.get_dropbox_file_info(customer,os.path.join(orders_retrieve_path,'received'),from_date=datetime.now()-timedelta(days=10))
         if queuedFiles:
-            or_df = pd.DataFrame(columns = ['order_id','ean','date_ordered','channel','qty_ordered','OR'])
-            pc_df = pd.DataFrame(columns = ['order_id','ean','date_shipped','qty_shipped','qty_variance','PC'])
+            stock_columns = ['order_id','ean','date_ordered','channel','qty_ordered','OR','date_shipped','qty_shipped','qty_variance','PC']
+            or_df = pd.DataFrame(columns = stock_columns)
+            pc_df = pd.DataFrame(columns = stock_columns)
             po_df = pd.DataFrame(columns = ['po_number','date_received','ean','qty_received'])
             for file_item in queuedFiles:
                 byte_stream = common.read_dropbox_bytestream('aemery',file_item['path_display'])
