7b17520442e811dfb1457345e76e872bea2d849a
mclwill <gary@mclarenwilliams.com.au>
2024-08-22 21:43:17 +1000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -42,7 +42,7 @@ for view_func in app.view_functions:
 
 def last_day_of_month(any_day):
     # The day 28 exists in every month. 4 days later, it's always next month
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    next_month = any_day.replace(day=28) + timedelta(days=4)
     # subtracting the number of the current day brings us back one month
     return next_month - datetime.timedelta(days=next_month.day)
 
