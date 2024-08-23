8e046362986536e8a9559411fc3c2d5a951c70e5
mclwill <gary@mclarenwilliams.com.au>
2024-08-23 23:05:40 +1000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -176,7 +176,7 @@ def serve_layout():
         stock_info_df.set_index('ean',inplace=True)
         common.logger.info(str(stock_info_df.dtypes) + '\n' + str(stock_info_df.index.dtype))
         common.logger.info(str(additional_purchases_df.dtypes) + str(additional_purchases_df.index.dtype))
-        stock_info_df.astype({'ean':str})
+        stock_info_df.index = stock_info_df.index.astype({'ean':str})
         stock_info_df = stock_info_df.join(additional_purchases_df)
         stock_info_df = stock_info_df.join(online_orders_prev_week_df)
         stock_info_df = stock_info_df.join(wholesale_orders_prev_week_df)
