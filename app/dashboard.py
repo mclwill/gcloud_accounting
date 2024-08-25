2b8b874852622ddd59986f48e04d7f4dced91ddb
mclwill <gary@mclarenwilliams.com.au>
2024-08-25 23:46:21 +1000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -513,7 +513,7 @@ def set_dropdown_options(season,v_base_start_date):
                         seasons.append(s)
             dff = dff[dff['season'].str.contains('|'.join(seasons))]
         return [{'label':x,'value':x} for x in dff['p_name'].unique()]
-    else 
+    else:
         return None
 
 
@@ -529,7 +529,7 @@ def set_dropdown_options(product,v_base_start_date):
         if product:
             dff = dff[dff['p_name'].isin(product)]
         return [{'label':x,'value':x} for x in dff['color'].unique()]
-    else 
+    else: 
         return None
 
 @dash_app.callback(
@@ -547,7 +547,7 @@ def set_dropdown_options(product,color,v_base_start_date):
         if color:
             dff = dff[dff['color'].isin(color)]
         return [{'label':x,'value':x} for x in dff['size'].unique()]
-    else 
+    else:
         return None
 
 
