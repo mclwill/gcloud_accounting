28b9f8de2daffe05d2aaebb087d48f3b3415262b
mclwill <gary@mclarenwilliams.com.au>
2024-08-27 13:09:00 +1000

    remove card from data table

--- app/dashboard.py
+++ app/dashboard.py
@@ -485,8 +485,8 @@ def serve_layout(base_stock_info_df,end_season_date):
                 ]),
             ], align='center'),
             dbc.Row([
-                dbc.Card([
-                    dbc.CardBody([
+                #dbc.Card([
+                #    dbc.CardBody([
                         dash_table.DataTable(
                             id='data_table',
                             columns=[col_title_mapping[i] for i in display_stock_info_df.columns],
@@ -503,8 +503,8 @@ def serve_layout(base_stock_info_df,end_season_date):
                             css=[dict(selector= "p", rule= "margin: 0; text-align: center")],
                             sort_action = 'native',
                         )
-                    ]),
-                ]),
+                #    ]),
+                #]),
             ]),
             dcc.Store(id='signal')
         ])
