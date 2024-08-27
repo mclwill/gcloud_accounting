751adec8390c7fd80e2483e610497c70557fff4f
mclwill <gary@mclarenwilliams.com.au>
2024-08-27 22:55:29 +1000

    d

--- app/dashboard.py
+++ app/dashboard.py
@@ -368,15 +368,27 @@ def serve_layout(base_stock_info_df,end_season_date):
                             ]),
                         ]),
                     ],className="border-0 bg-transparent"),
-                    width = {"size":3, 'offset':1}
+                    width = {"size":3}
                 ),
-                dbc.Col([
-                    dbc.Button("Download CSV", id="btn_csv",color='light',size='lg'),
-                    dcc.Download(id="download-dataframe-csv"),
-                    ],width={"size":2}),
                 dbc.Col(
-                    dbc.Button("Logout",href='/logout',color='light',size='lg',external_link=True,),
-                    width={"size":1}
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Div([
+                                dbc.Button("Download CSV", id="btn_csv",color='light',size='lg'),
+                                dcc.Download(id="download-dataframe-csv"),
+                            ]),
+                        ]),
+                    ],className="border-0 bg-transparent"),
+                    width={"size":2}),
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Div([
+                                dbc.Button("Logout",href='/logout',color='light',size='lg',external_link=True,)
+                            ]),
+                        ]),
+                    ],className="border-0 bg-transparent"),
+                    width={"size":2}
                 )
             ]),
             dbc.Row([
