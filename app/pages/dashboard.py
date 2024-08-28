c0f4af63ce48901839219008e2e2e1c24e69a2d0
mclwill <gary@mclarenwilliams.com.au>
2024-08-28 20:48:46 +1000

    d

--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -887,9 +887,10 @@ def update_table(v_season,v_category,v_sub_cat,v_product,v_color,v_size,v_shortc
     Input('data_table','selected_rows'),
 )
 def updated_selected_rows(v_rows):
-    common.logger.info(str(v_rows))
     if v_rows:
-        return '/dashboard/graphs?plots=' + ','.join(map(str,v_rows))
+        plots_str = ','.join(map(str,v_rows))
+        common.logger.info(str(plots_str))
+        return '/dashboard/graphs?plots=' + plots_str
 
 '''@callback (
     Input('btn_graphs','n_clicks'),
