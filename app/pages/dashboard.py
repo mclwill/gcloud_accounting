bb74af145b742739b99752a5a4777006b96eace4
mclwill <gary@mclarenwilliams.com.au>
2024-08-28 20:05:04 +1000

    d

--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -554,7 +554,7 @@ def layout(**kwargs):
                             columns=[col_title_mapping[i] for i in display_columns if i in col_title_mapping.keys()],
                             #columns=[{"name": col_title_mapping[i], "id": i, 'presentation':'markdown'} if ('markdown' in i) else {"name": col_title_mapping[i], "id": i} for i in display_stock_info_df.columns],
                             data=display_stock_info_df.to_dict("records"),
-                            rows_selectable = 'multi',
+                            row_selectable = 'multi',
                             selected_rows = [],
                             style_cell_conditional = [
                                 {
