5c9458f87ed824f96e57e475f74c77eb04d92a7a
mclwill <gary@mclarenwilliams.com.au>
2024-08-28 15:29:07 +1000

    d

--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -6,9 +6,7 @@ from flask_login import login_required
 import dash
 #import dash_html_components as html
 #import dash_core_components as dcc
-from dash import html
-from dash import dcc
-from dash import dash_table
+from dash import html, dcc, callback, dash_table
 import dash_bootstrap_components as dbc
 #import dash_table
 from dash.dependencies import Input, Output, State
