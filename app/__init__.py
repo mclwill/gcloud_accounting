6c104afc5ff346be26060b14e5164dfb271b8bf7
mclwill <gary@mclarenwilliams.com.au>
2024-08-28 15:18:32 +1000

    d

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 from FlaskApp.app import auth
 from FlaskApp.app import views
-from FlaskApp.app.pages import dashboard
+
 
 with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
     print('__init__.py')
@@ -16,6 +16,8 @@ with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
 external_stylesheets = [dbc.themes.BOOTSTRAP,'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 dash_app = dash.Dash(server=app,use_pages=True,external_stylesheets=external_stylesheets,routes_pathname_prefix="/dashboard/") #previousy 'routes_pathname_prefix'
+
+from FlaskApp.app.pages import dashboard
 
 dash_app.layout = html.Div([
     dash.page_container
