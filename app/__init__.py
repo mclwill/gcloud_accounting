d5beb2b4548ba4dc899f92fc0755353cf4767bae
mclwill <gary@mclarenwilliams.com.au>
2024-08-29 22:40:49 +1000

    d

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_login import login_required
 import sys
 
-
-
 app = Flask(__name__)
 
 
@@ -12,8 +10,10 @@ from FlaskApp.app import views
 
 #with open('/var/log/cd-uphance/app.log', 'a') as sys.stdout:
 #    print('__init__.py')
-from FlaskApp.app import dash_main
 
+dash_app = dash.Dash(server=app,use_pages=True,external_stylesheets=external_stylesheets,routes_pathname_prefix="/dashboard/") #previousy 'routes_pathname_prefix'
+
+from FlaskApp.app import dash_main
 
 for view_func in app.view_functions:
     if view_func.startswith(dash_app.config['routes_pathname_prefix']):
--- app/dash_main.py
+++ app/dash_main.py
@@ -4,11 +4,11 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = [dbc.themes.BOOTSTRAP,'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-dash_app = dash.Dash(server=app,use_pages=True,external_stylesheets=external_stylesheets,routes_pathname_prefix="/dashboard/",suppress_call_exceptions=True) #previousy 'routes_pathname_prefix'
 
-dash_app.layout = html.Div([
+
+dash.get_app().layout = html.Div([
     dash.page_container
 ])
 
-dash_app.validation_layout = html.Div([
-	graphs.graphs_layout])
+#dash_app.get_app.validation_layout = html.Div([
+#	graphs.graphs_layout])
