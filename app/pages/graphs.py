bd1e4e8defaa11c5c1d62ff35f7720d215df1021
mclwill <gary@mclarenwilliams.com.au>
2024-08-29 15:39:04 +1000

    d

--- app/pages/graphs.py
+++ app/pages/graphs.py
@@ -7,41 +7,41 @@ from FlaskApp.app.data_store import get_data_from_globals
 dash.register_page(__name__)
 
 def layout(**kwargs):
-	
-	try:
-		data = kwargs.pop('data',None)
-	
-		if len(data) > 0:
-	        data_df = pd.DataFrame.from_dict(data)
-	        data_cols = data_df.columns.tolist()
-	        if 'color' in data_cols:
-	            if 'size' in data_cols:
-	                keys = ['p_name','color','size']
-	            else:
-	                keys = ['p_name','color']
-	        elif 'size' in data_cols:
-	            keys = ['p_name','size']
-	        else:
-	            keys = False
+    
+    try:
+        data = kwargs.pop('data',None)
+    
+        if len(data) > 0:
+            data_df = pd.DataFrame.from_dict(data)
+            data_cols = data_df.columns.tolist()
+            if 'color' in data_cols:
+                if 'size' in data_cols:
+                    keys = ['p_name','color','size']
+                else:
+                    keys = ['p_name','color']
+            elif 'size' in data_cols:
+                keys = ['p_name','size']
+            else:
+                keys = False
 
-		    stock_df = get_data_from_globals()[0].copy()
+            stock_df = get_data_from_globals()[0].copy()
 
-			if keys: # if multiple columns then use index matching approach
-				idx_stock = stock_df.set_index(keys).index
-				idx_data = data_df.set_index(keys).index
-				dff = stock_df[idx_stock.isin(idx_data)]
-			else: #if just p_name then filter on p_name
-				dff = stock_df[stock_df['p_name'].isin(data_df['p_name'].unique().to_list())]
+            if keys: # if multiple columns then use index matching approach
+                idx_stock = stock_df.set_index(keys).index
+                idx_data = data_df.set_index(keys).index
+                dff = stock_df[idx_stock.isin(idx_data)]
+            else: #if just p_name then filter on p_name
+                dff = stock_df[stock_df['p_name'].isin(data_df['p_name'].unique().to_list())]
 
-			return html.Div([
-		        dcc.Graph(id='graph_fig',fig = px.line(dff,x='date',y='available_to_sell'))
-		    ])
-		else:
-			return html.Div([
-		        html.Div('No data to display')
-		    ])
+            return html.Div([
+                dcc.Graph(id='graph_fig',fig = px.line(dff,x='date',y='available_to_sell'))
+            ])
+        else:
+            return html.Div([
+                html.Div('No data to display')
+            ])
 
-	except Exception as ex:
+    except Exception as ex:
         tb = traceback.format_exc()
         common.logger.warning('Error Graphing Info' + '\nException Info: ' + str(ex) + '/nTraceback Info: ' + str(tb))
         return html.Div(
