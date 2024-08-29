51c9c6c46ea677f65784349f2777f10b668222b3
mclwill <gary@mclarenwilliams.com.au>
2024-08-29 21:45:41 +1000

    d

--- app/dash_main.py
+++ app/dash_main.py
@@ -1,5 +1,9 @@
+import dash
 from dash import Dash, html, dcc
-import dash_bootstrap_components as dbc
+
+external_stylesheets = [dbc.themes.BOOTSTRAP,'https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+dash_app = dash.Dash(server=app,use_pages=True,external_stylesheets=external_stylesheets,routes_pathname_prefix="/dashboard/",suppress_call_exceptions=True) #previousy 'routes_pathname_prefix'
 
 dash_app.layout = html.Div([
     dash.page_container
