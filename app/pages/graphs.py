b7ac193c6b7721c58439fbb20ff60e406775c34d
mclwill <gary@mclarenwilliams.com.au>
2024-08-30 08:32:58 +1000

    d

--- app/pages/graphs.py
+++ app/pages/graphs.py
@@ -10,22 +10,86 @@ import plotly.express as px
 import traceback
 import pandas as pd
 import json
+import urllib.parse
 
 import FlaskApp.app.common as common
 from FlaskApp.app.data_store import get_data_from_globals
 
 dash.register_page(__name__)
 
+layout = html.Div([
+    dbc.Container(
+        children= [
+            dbc.Row([
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.H1("Dashboard"),
+                            html.P('''
+                                 This is a dashboard for A.Emery
+                                 '''),
+                        ]),   
+                    ],className="border-0 bg-transparent"),
+                    width={"size":3} 
+                ),
+                dbc.Col(
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Div([
+                                dbc.Button("LOGOUT",href='/logout',color='light',size='lg',external_link=True,)
+                            ]),
+                        ]),
+                    ],className="border-0 bg-transparent"),
+                    width={"size":1,'offset':8}
+                )
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    html.Div([
+                        html.Div(id='dd-output-container-graph',children='Data Update Complete')
+                    ],style={'backgroundColor':'red','color':'white'})
+                ]),
+            ], align='center'),
+            dbc.Row([
+                dbc.Col(
+                    html.Div([
+                        dcc.Graph(id='graph-px-alt')
+                    ])
+                )
+            ]),
+            dbc.Row([
+                dbc.Col(
+                    dcc.RadioItems(
+                        ['Absolute','Normalised'],
+                         'Absolute',
+                         id ='graph-type-alt',
+                         inline=True,
+                         style = {'padding':'10px'}
+                    )
+                ,width={'size':1})
+            ],justify='center')
+        ],fluid=True),
+    #dcc.Store(id = 'clientside-figure-store-px'),
+    dcc.Location(id='url'),
+    dcc.Store(id='df-store'),
+    dcc.Store(id='name-text')
+])
 
-def layout(data=None,**kwargs):
-    global df_graph, fig, name_text
-
+@callback(
+    [Output('df-store', 'data'),
+     Output('name-text','data')],
+    Input('url', 'search')
+)
+def get_query(url):
+    
     try:
-        #data = kwargs.pop('data',None)
-        #common.logger.info(str(data))
-        if data:
-            data = json.loads(data)
-            if data:
+        
+        if url:
+            if url[1:].startswith('data'):  #dcc.Location 'search' return query string including '?' - so skip over that
+                data = url[1:].replace('data=','')
+                #common.logger.info('URL call back reached' + str(data))
+                data = json.loads(urllib.parse.unquote(data)) #need to decode url string before sending through json decoder
+
                 #common.logger.info(str(data))
                 data_df = pd.DataFrame.from_records(data)
                 data_cols = data_df.columns.tolist()
@@ -82,76 +146,27 @@ def layout(data=None,**kwargs):
                 fig.update_layout(
                     height = 600
                 )
-                #common.logger.info(str(df_grouped[['date','available_to_sell']].head()))
-                return html.Div([
-                    dbc.Container(
-                        children= [
-                            dbc.Row([
-                                dbc.Col(
-                                    dbc.Card([
-                                        dbc.CardBody([
-                                            html.H1("Dashboard"),
-                                            html.P('''
-                                                 This is a dashboard for A.Emery
-                                                 '''),
-                                        ]),   
-                                    ],className="border-0 bg-transparent"),
-                                    width={"size":3} 
-                                ),
-                                dbc.Col(
-                                    dbc.Card([
-                                        dbc.CardBody([
-                                            html.Div([
-                                                dbc.Button("LOGOUT",href='/logout',color='light',size='lg',external_link=True,)
-                                            ]),
-                                        ]),
-                                    ],className="border-0 bg-transparent"),
-                                    width={"size":1,'offset':8}
-                                )
-                            ]),
-                            dbc.Row([
-                                dbc.Col(
-                                    html.Div([
-                                        dcc.Graph(id='graph-px',figure = fig)
-                                    ])
-                                )
-                            ]),
-                            dbc.Row([
-                                dbc.Col(
-                                    dcc.RadioItems(
-                                        ['Absolute','Normalised'],
-                                         'Absolute',
-                                         id ='graph-type'
-                                    )
-                                )
-                            ])
-                        ],fluid=True),
-                    #dcc.Store(id = 'clientside-figure-store-px'),
-                ])
 
-
-        #return error if fall through to here
-
-        return html.Div([
-            html.Div('No data to display')
-        ])
+                return df_graph.to_json(date_format='iso', orient='split'), name_text
 
     except Exception as ex:
         tb = traceback.format_exc()
-        common.logger.warning('Error Graphing Info' + '\nException Info: ' + str(ex) + '/nTraceback Info: ' + str(tb))
-        return html.Div(
-                html.P('Error processing graph')
-        )
+        common.logger.warning('Error Process Dashboard Layout' + '\nException Info: ' + str(ex) + '/nTraceback Info: ' + str(tb))
+
 
 @callback(
-    Output('graph-px', 'figure'),
-    Input('graph-type', 'value')
+    Output('graph-px-alt', 'figure'),
+    [Input('df-store','data'),
+     Input('graph-type-alt', 'value'),
+     Input('name-text','data')],
+     running=[(Output("dd-output-container-graph","children"),'Data Being Updated.....Please Wait', 'Data Update Complete'),
+                 (Output("dd-output-container-graph","style"),{'backgroundColor':'red','color':'white'},{'backgroundColor':'white','color':'black'})]
 )
-def update_store_data(graph_type):
-    global df_graph,name_text
+def update_figure(df_graph,graph_type,name_text):
     
     try:
-        common.logger.info('call back reached' + str(graph_type))
+        #common.logger.info('call back reached' + str(graph_type) + str(name_text) + str(df_graph))
+        df_graph = pd.read_json(df_graph, orient='split')
         if graph_type == 'Absolute':
             plot_cols = [x for x in df_graph.columns.tolist() if '_norm' not in x]
             dff = df_graph[plot_cols]
@@ -166,8 +181,8 @@ def update_store_data(graph_type):
     except Exception as ex:
         tb = traceback.format_exc()
         common.logger.warning('Error Process Dashboard Layout' + '\nException Info: ' + str(ex) + '/nTraceback Info: ' + str(tb))
-'''
-clientside_callback(
+
+'''clientside_callback(
     """
     function(data) {
         return {
