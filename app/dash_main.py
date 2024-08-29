aafbf1b0570950819ffea0a55eae091fdcbd4262
mclwill <gary@mclarenwilliams.com.au>
2024-08-29 22:55:00 +1000

    d

--- app/dash_main.py
+++ app/dash_main.py
@@ -7,5 +7,5 @@ dash.get_app().layout = html.Div([
     dash.page_container
 ])
 
-#dash_app.get_app.validation_layout = html.Div([
-#	graphs.graphs_layout])
+dash_app.get_app.validation_layout = html.Div([
+	graphs.layout])
