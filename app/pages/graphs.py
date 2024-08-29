14b0db840d18fe0d47642aad48416204ef45d3cd
mclwill <gary@mclarenwilliams.com.au>
2024-08-29 17:38:06 +1000

    d

--- app/pages/graphs.py
+++ app/pages/graphs.py
@@ -53,7 +53,7 @@ def layout(**kwargs):
                 df_grouped = dff[['date'] + keys + ['available_to_sell']].groupby(keys + ['date']).agg({'available_to_sell':'sum'}).reset_index()
                 df_grouped['Name'] = df_grouped[keys].apply(lambda x: ' - '.join(x.astype(str)),axis=1)
                 
-                df_graph = df_grouped.pivot(index='date',cols='Name',values='available_to_sell').reset_index()
+                df_graph = df_grouped.pivot(index='date',columns='Name',values='available_to_sell').reset_index()
 
                 fig = px.line(df_graph,x='date',y=df_graph.columns,hover_data={'date':'%Y-%m-%d'})
                 #common.logger.info(str(df_grouped[['date','available_to_sell']].head()))
